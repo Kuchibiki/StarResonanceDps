4a0b78be3b548c731bb031713cb6a3db805fafc1
ihonliu <ihonliu@gmail.com>
2026-02-04 16:27:45 +0800

    style: Change MarqueeTextBehavior style

--- StarResonanceDpsAnalysis.WPF/Behaviors/MarqueeTextBehavior.cs
+++ StarResonanceDpsAnalysis.WPF/Behaviors/MarqueeTextBehavior.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Animation;
@@ -9,29 +8,31 @@ namespace StarResonanceDpsAnalysis.WPF.Behaviors;
 public class MarqueeTextBehavior : Behavior<FrameworkElement>
 {
     public static readonly DependencyProperty ContainerProperty =
-        DependencyProperty.Register("Container", typeof(FrameworkElement), typeof(MarqueeTextBehavior), new PropertyMetadata(null, OnContainerChanged));
-
-    public FrameworkElement Container
-    {
-        get { return (FrameworkElement)GetValue(ContainerProperty); }
-        set { SetValue(ContainerProperty, value); }
-    }
+        DependencyProperty.Register(nameof(Container), typeof(FrameworkElement), typeof(MarqueeTextBehavior),
+            new PropertyMetadata(null, OnContainerChanged));
 
     public static readonly DependencyProperty IsAnimationEnabledProperty =
-        DependencyProperty.Register("IsAnimationEnabled", typeof(bool), typeof(MarqueeTextBehavior), new PropertyMetadata(true, OnIsAnimationEnabledChanged));
+        DependencyProperty.Register(nameof(IsAnimationEnabled), typeof(bool), typeof(MarqueeTextBehavior),
+            new PropertyMetadata(true, OnIsAnimationEnabledChanged));
+
+    private Storyboard? _storyboard;
+
+    public FrameworkElement? Container
+    {
+        get => (FrameworkElement)GetValue(ContainerProperty);
+        set => SetValue(ContainerProperty, value);
+    }
 
     public bool IsAnimationEnabled
     {
-        get { return (bool)GetValue(IsAnimationEnabledProperty); }
-        set { SetValue(IsAnimationEnabledProperty, value); }
+        get => (bool)GetValue(IsAnimationEnabledProperty);
+        set => SetValue(IsAnimationEnabledProperty, value);
     }
 
     private static void OnIsAnimationEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
         ((MarqueeTextBehavior)d).UpdateAnimation();
     }
-
-    private Storyboard _storyboard;
 
     private static void OnContainerChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
@@ -58,12 +59,13 @@ public class MarqueeTextBehavior : Behavior<FrameworkElement>
         base.OnDetaching();
     }
 
-    private void SetupListeners(FrameworkElement oldContainer, FrameworkElement newContainer)
+    private void SetupListeners(FrameworkElement? oldContainer, FrameworkElement? newContainer)
     {
         if (oldContainer != null)
         {
             oldContainer.SizeChanged -= OnSizeChanged;
         }
+
         if (newContainer != null)
         {
             newContainer.SizeChanged += OnSizeChanged;
@@ -91,8 +93,8 @@ public class MarqueeTextBehavior : Behavior<FrameworkElement>
             return;
         }
 
-        double contentWidth = AssociatedObject.ActualWidth;
-        double containerWidth = Container.ActualWidth;
+        var contentWidth = AssociatedObject.ActualWidth;
+        var containerWidth = Container.ActualWidth;
 
         // If not loaded effectively
         if (containerWidth <= 0 || contentWidth <= 0) return;
@@ -115,24 +117,28 @@ public class MarqueeTextBehavior : Behavior<FrameworkElement>
         // For simplicity, we restart.
         StopAnimation();
 
-        double scrollDistance = contentWidth - containerWidth;
-        double speed = 30.0; // pixels per second
-        double scrollTime = scrollDistance / speed;
+        var scrollDistance = contentWidth - containerWidth;
+        var speed = 30.0; // pixels per second
+        var scrollTime = scrollDistance / speed;
         if (scrollTime < 1.0) scrollTime = 1.0;
 
         var sb = new Storyboard();
-        var animation = new DoubleAnimationUsingKeyFrames();
-        animation.RepeatBehavior = RepeatBehavior.Forever;
-        
+        var animation = new DoubleAnimationUsingKeyFrames
+        {
+            RepeatBehavior = RepeatBehavior.Forever
+        };
+
         // 1. Start at 0, hold for 2s
         animation.KeyFrames.Add(new DiscreteDoubleKeyFrame(0, KeyTime.FromTimeSpan(TimeSpan.Zero)));
         animation.KeyFrames.Add(new LinearDoubleKeyFrame(0, KeyTime.FromTimeSpan(TimeSpan.FromSeconds(2))));
 
         // 2. Scroll to -scrollDistance
-        animation.KeyFrames.Add(new LinearDoubleKeyFrame(-scrollDistance, KeyTime.FromTimeSpan(TimeSpan.FromSeconds(2 + scrollTime))));
+        animation.KeyFrames.Add(new LinearDoubleKeyFrame(-scrollDistance,
+            KeyTime.FromTimeSpan(TimeSpan.FromSeconds(2 + scrollTime))));
 
         // 3. Hold at end for 2s
-        animation.KeyFrames.Add(new LinearDoubleKeyFrame(-scrollDistance, KeyTime.FromTimeSpan(TimeSpan.FromSeconds(2 + scrollTime + 2))));
+        animation.KeyFrames.Add(new LinearDoubleKeyFrame(-scrollDistance,
+            KeyTime.FromTimeSpan(TimeSpan.FromSeconds(2 + scrollTime + 2))));
 
         // 4. Reset happens by loop (Discrete to 0 at start of next loop)
 
@@ -146,10 +152,8 @@ public class MarqueeTextBehavior : Behavior<FrameworkElement>
 
     private void StopAnimation()
     {
-        if (_storyboard != null)
-        {
-            _storyboard.Stop();
-            _storyboard = null;
-        }
+        if (_storyboard == null) return;
+        _storyboard.Stop();
+        _storyboard = null;
     }
 }
